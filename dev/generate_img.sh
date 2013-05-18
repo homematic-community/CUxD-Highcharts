22ca13903c8c326c6fa343fdcb284cfbf0de5e69
hobbyquaker <hobbyquaker@gmail.com>
2013-05-18 13:40:27 +0200

    1.3.5 Release

--- dev/generate_img.sh
+++ dev/generate_img.sh
@@ -7,6 +7,10 @@ cp dev/cuxd-highcharts tmp/
 cp dev/update_script tmp/
 cp -a cuxchart/* tmp/cuxchart
 
+awk '/cuxchart.js/{gsub(/cuxchart.js/, "cuxchart.min.js")};{print}' cuxchart/index.html > tmp/cuxchart/index.html
+java -jar dev/yuicompressor-2.4.7.jar --charset ISO-8859-1 cuxchart/js/cuxchart.js >> tmp/cuxchart/js/cuxchart.min.js
+
 cd tmp
+rm cuxchart/js/cuxchart.js
 
 tar -czvf ../cuxchart.tar.gz *
